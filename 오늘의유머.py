80a4604f6cb28c943bc61a8aab1a7b508f273b0c
papasmf1 <papasmf1@gmail.com>
2023-10-26 16:28:51 +0900

    PyQt 연습2

--- 오늘의유머.py
+++ 오늘의유머.py
@@ -6,9 +6,11 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
-for n in range(0,10):
-        #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+#파일에 저장
+f = open("c:\\work\\today.txt", "wt", encoding="utf-8")
+for n in range(1, 11):
+        #오늘의 유머 베스트 게시판 
+        data ='https://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
@@ -17,18 +19,19 @@ for n in range(0,10):
         #한글이 깨지는 경우
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
-        # <span class="subject_fixed" data-role="list-title-text" title="아이폰 13프로 256 블루 (북미판) 새로 리퍼 받은 S급">
-        # 		아이폰 13프로 256 블루 (북미판) 새로 리퍼 받은 S급
-        # </span>
-
+        list = soup.find_all('td', attrs={'class':'subject'})
+        #<td class="subject">
+        # <a href="/board/view.php?table=bestofbest">우리나라 지금 현재 전쟁나면 필패인 이유 추가</a><span class="list_memo_count_span"> [18]
+        #</td>
         for item in list:
+                #에러처리(try ~ catch)
                 try:
-                        title = item.text.strip()
-                        print(title)
-                        # if (re.search('아이폰', title)):
-                        #         print(title.strip())
-                        #         print('https://www.clien.net'  + item['href'])
+                        title = item.find("a").text.strip() 
+                        if re.search("한국", title):
+                            print(title)
+                            f.write(f"{title}\n")
                 except:
                         pass
         
+
+f.close() 
