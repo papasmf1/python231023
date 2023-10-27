c990efba85c639d2f76e8eb741a2960d08a5c40b
papasmf1 <papasmf1@gmail.com>
2023-10-27 09:27:02 +0900

    PyQT 연습 완료

--- DemoForm2.py
+++ DemoForm2.py
@@ -2,6 +2,11 @@
 import sys
 from PyQt5.QtWidgets import * 
 from PyQt5 import uic
+#웹서버에 요청
+import requests
+#크롤링
+from bs4 import BeautifulSoup
+
 
 #디자인 파일 로딩(파일명 변경)
 form_class = uic.loadUiType("DemoForm2.ui")[0]
@@ -13,7 +18,23 @@ class DemoForm(QMainWindow, form_class):
         self.setupUi(self)
     #슬롯 메서드 
     def firstClick(self):
-        self.label.setText("첫번째 버튼")
+        url = "https://www.daangn.com/fleamarket/"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        f = open("c:\\work\\dangn.txt", "wt", encoding="utf-8")
+        posts = soup.find_all("div", attrs={"class":"card-desc"})
+        for post in posts:
+            title = post.find("h2", attrs={"class":"card-title"})
+            price = post.find("div", attrs={"class":"card-price"})
+            addr = post.find("div", attrs={"class":"card-region-name"})
+            title = title.text.strip().replace("\n", "")
+            price = price.text.strip().replace("\n", "")
+            addr = addr.text.strip().replace("\n", "") 
+            print("{0}, {1}, {2}".format(title, price, addr))
+            #f를 붙이고 변수명 넘기기
+            f.write(f"{title}, {price}, {addr}\n")
+        f.close() 
+        self.label.setText("당근 크롤링 완료~~")
     def secondClick(self):
         self.label.setText("두번째 버튼~~")
     def thirdClick(self):
